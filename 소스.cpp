ab65834403cb506afe182fa0a22c7ffa89a7f71e
jangjusung <81299430+jangjusung@users.noreply.github.com>
2021-06-23 15:21:32 +0900

    Add files via upload
    -정수 배열 (크기10)
    임의의 위치에 1출력 나머지 0
    지연 시간 500을 기준으로 임의의 위치에 1이동 
    화면 클리어 
    무한 루프 사용

--- 소스.cpp
+++ 소스.cpp
@@ -1,46 +1,28 @@
 #include <iostream>
 #include <time.h>
 #include <stdlib.h>
+#include <windows.h>
 
 using namespace std;
 
-int A[10];
-int B[10];
+int A[10] = { 0, };
 
 void arr()
 {
-	int count = 0;
-	int Arand, Brand;
-	for (int i = 0; i < 10; i++)
+	int Arand;
+	int stop = 1;
+	while (stop)
 	{
 		Arand = rand() % 10;
-		A[i] = Arand;
-	}
-
-	for (int i = 0; i < 10; i++)
-	{
-		Brand = rand() % 10;
-		B[i] = Brand;
-	}
-	cout << "A배열:";
-	for (int i = 0; i < 10; i++)
-	{
-		cout << A[i] << " ";
-	}
-	cout << endl << "B배열:";
-	for (int i = 0; i < 10; i++)
-	{
-		cout << B[i] << " ";
-	}
-	for (int i = 0; i < 10; i++)
-	{
-		if (A[i] == B[i])
+		A[Arand] = 1;
+		for (int i = 0; i < 10; i++)
 		{
-			count = count + 10;
+			cout << A[i];
 		}
+		A[Arand] = 0;
+		Sleep(500);
+		system("cls");
 	}
-	cout << endl;
-	cout << "두 배율의 닮은 비율=" << count << "%";
 }
 int main()
 {
